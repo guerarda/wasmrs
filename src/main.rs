0c79461f54a61b3b3febb9bb307fb252d22a3544
guerarda <guerarda@users.noreply.github.com>
2026-01-07 00:01:59 +0000

    Add test for add and fib

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,66 @@ fn main() -> anyhow::Result<()> {
     let mut runtime = Runtime::default();
     let mh = runtime.load_module(&bytes)?;
 
-    let r = runtime.invoke(mh, "fib", &[Value::I32(20)]);
-    dbg!(r);
+    dbg!(runtime);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_wasm() -> anyhow::Result<()> {
+        let bytes = fs::read("tests/fixtures/add.wasm")?;
+        let mut runtime = Runtime::default();
+        let mh = runtime.load_module(&bytes)?;
+
+        let cases = [
+            // (a + b = c)
+            (1, 2, 3),
+            (2, 2, 4),
+            (-2, 2, 0),
+        ];
+
+        for (a, b, c) in cases {
+            let r = runtime.invoke(mh, "add", &[Value::I32(a), Value::I32(b)]);
+
+            assert_eq!(r.len(), 1);
+            match r[0] {
+                Value::I32(v) => assert_eq!(v, c, "{} + {} = {}", a, b, c),
+                _ => panic!(),
+            }
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_fib_wasm() -> anyhow::Result<()> {
+        let bytes = fs::read("tests/fixtures/fib.wasm")?;
+        let mut runtime = Runtime::default();
+        let mh = runtime.load_module(&bytes)?;
+
+        let cases = [
+            // fib(a) = b
+            (0, 0),
+            (1, 1),
+            (7, 13),
+            (10, 55),
+            (20, 6765),
+        ];
+
+        for (a, b) in cases {
+            let r = runtime.invoke(mh, "fib", &[Value::I32(a)]);
+
+            assert_eq!(r.len(), 1);
+            match r[0] {
+                Value::I32(v) => assert_eq!(v, b, "F({}) = {}", a, b),
+                _ => panic!(),
+            }
+        }
+
+        Ok(())
+    }
 }
