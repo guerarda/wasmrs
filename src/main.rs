c8ebdd5e02fbabd194c1bdbb14c255ee6b4144b9
guerarda <guerarda@users.noreply.github.com>
2026-01-20 14:55:03 +0000

    Add test for div with assert trap

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,10 @@ fn main() -> anyhow::Result<()> {
 
 #[cfg(test)]
 mod tests {
+    use std::result;
+
     use super::*;
-    use wasmrs::runtime::value::Value;
+    use wasmrs::{Error, runtime::value::Value};
 
     #[test]
     fn test_add_wasm() -> anyhow::Result<()> {
@@ -33,6 +35,11 @@ mod tests {
             (1, 2, 3),
             (2, 2, 4),
             (-2, 2, 0),
+            // wrap-around
+            (0x7fffffff, 1, 0x80000000u32 as i32),
+            (0x80000000u32 as i32, -1, 0x7fffffff),
+            (0x80000000u32 as i32, 0x80000000u32 as i32, 0),
+            (0x3fffffff, 1, 0x40000000),
         ];
 
         for (a, b, c) in cases {
@@ -97,6 +104,58 @@ mod tests {
                 Value::I32(v) => assert_eq!(v, c, "{} * {} = {}", a, b, c),
                 _ => panic!(),
             }
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_divs_wasm() -> anyhow::Result<()> {
+        let bytes = fs::read("tests/fixtures/div_s.wasm")?;
+        let mut runtime = Runtime::default();
+        let mh = runtime.load_module(&bytes)?;
+
+        let cases = [
+            // (a / b = c)
+            (1, 1, 1),
+            (0, 1, 0),
+            (0, -1, 0),
+            (-1, -1, 1),
+            (0x80000000u32 as i32, 2, 0xc0000000u32 as i32),
+            (0x80000001u32 as i32, 1000, 0xffdf3b65u32 as i32),
+        ];
+
+        for (a, b, c) in cases {
+            let r = runtime.invoke(mh, "div", &[Value::I32(a), Value::I32(b)])?;
+
+            assert_eq!(r.len(), 1);
+            match r[0] {
+                Value::I32(v) => assert_eq!(v, c, "{} / {} = {}", a, b, c),
+                _ => panic!(),
+            }
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_divs_wasm_trap() -> anyhow::Result<()> {
+        let bytes = fs::read("tests/fixtures/div_s.wasm")?;
+        let mut runtime = Runtime::default();
+        let mh = runtime.load_module(&bytes)?;
+
+        let cases = [
+            // (a / b)
+            (1, 0),
+            (0, 0),
+            (0x80000000u32 as i32, -1),
+            (0x80000000u32 as i32, 0),
+        ];
+
+        for (a, b) in cases {
+            let r = runtime.invoke(mh, "div", &[Value::I32(a), Value::I32(b)]);
+
+            assert!(matches!(r, Err(Error::Trap)));
         }
 
         Ok(())
