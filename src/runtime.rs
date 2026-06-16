bd5d1d100db24461273cf5838cf66e4ca148c638
guerarda <guerarda@users.noreply.github.com>
2026-06-16 10:56:07 +0100

    clippy fixes

--- src/runtime.rs
+++ src/runtime.rs
@@ -494,7 +494,7 @@ impl Runtime {
             .module_registry
             .resolve_export(module, fn_name)
             .ok_or(RuntimeError::internal("unknown export"))?;
-        let funcaddr: FuncAddr = (&ev)
+        let funcaddr: FuncAddr = ev
             .as_func()
             .ok_or(RuntimeError::internal("export is not a function"))?;
         let arity = self.store.functions.get(funcaddr).ftype.results.len();
