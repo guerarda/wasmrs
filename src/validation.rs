27b03cbef4be8f96db7a02bcf8b6b93e61de26af
guerarda <guerarda@users.noreply.github.com>
2026-06-29 21:12:04 +0100

    table.grow and table.fill check the actual value of the table
    org-id: BF7E814E-B5C7-45E1-8151-E5BDE3A68757

--- src/validation.rs
+++ src/validation.rs
@@ -1121,20 +1121,24 @@ impl Validator {
                 }
                 Instruction::TableGrow(idx) => {
                     // [t at] -> [at]
+                    let tabletype = Self::table_type_at(module, *idx)?;
+                    self.pop_val_expect(tabletype.elemtype.into())?;
                     self.pop_val_expect(ValueType::I32)?;
-                    self.pop_val_expect(ValueType::I32)?;
-                    Self::table_type_at(module, *idx)?;
 
                     self.push_val(ValueType::I32);
                 }
                 Instruction::TableSize(idx) => {
+                    // [] -> [at]
                     Self::table_type_at(module, *idx)?;
                     self.push_val(ValueType::I32);
                 }
                 Instruction::TableFill(idx) => {
                     // [at t at] -> []
                     self.pop_val_expect(ValueType::I32)?;
-                    self.pop_val_expect(ValueType::I32)?;
+
+                    let tabletype = Self::table_type_at(module, *idx)?;
+                    self.pop_val_expect(tabletype.elemtype.into())?;
+
                     self.pop_val_expect(ValueType::I32)?;
                     Self::table_type_at(module, *idx)?;
                 }
